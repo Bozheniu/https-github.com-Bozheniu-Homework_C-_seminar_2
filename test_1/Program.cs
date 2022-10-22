ad93a0a20ae78c6f678f8392552b50d6c06daeda
Bozheniu <bozheniu@gmail.com>
2022-10-23 01:11:47 +0200

    Решение задачи номер 10

--- test_1/Program.cs
+++ test_1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 10: Напишите программу, которая принимает на вход
+трёхзначное число и на выходе показывает вторую цифру этого числа. */
+
+int GetLastDigit(int number)
+{
+    return (number % 100) / 10;
+}
+
+int testNumber = 789;
+Console.WriteLine(GetLastDigit(testNumber));
